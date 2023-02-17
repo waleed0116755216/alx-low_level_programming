3b31a5731e1ab9dd2c73d26d4a98cf66a1c2aa0c
waleed0116755216 <waleedraslan1989#gmail.com>
2023-02-16 16:24:54 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 
 /**
  * *main - prints "Programming is like building a multilingual 
- * puzzle, followed by a newline."
- * return 0.
+ * puzzle, followed by a newline.
+ * Return:Always 0.
  */
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
