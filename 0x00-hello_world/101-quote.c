29d1b5d173a6ec74d3eebb17ea09700792369a4b
waleed0116755216 <waleedraslan1989#gmail.com>
2023-02-18 13:55:51 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
  
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
-  * followed by a new line, to the standard error.
-  * Return: Always 1.
-  */
+ * followed by a new line, to the standard error.
+ * Return: Always 1.
+ */
 
 int main(void)
 
