0c3201f22af261409e0554008de380c39b80c7ad
waleed0116755216 <waleedraslan1989#gmail.com>
2023-02-19 05:12:31 -0800

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 
 /**
- * *main - prints "Programming is like building a multilingual 
+ * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline.
  * Return: Always 0.
  */
+
 int main(void)
 
 {
